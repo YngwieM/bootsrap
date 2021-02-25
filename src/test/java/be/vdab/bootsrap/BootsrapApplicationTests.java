065b251340e83b6b389d8009c65501cbf9d80485
yngwie <yngwie_mariman@hotmail.com>
2021-02-25 11:56:40 +0100

    Initial commit

---
+++ src/test/java/be/vdab/bootsrap/BootsrapApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.bootsrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootsrapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
