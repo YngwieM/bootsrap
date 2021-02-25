92ba46e82ea1b917462cbcc1889375e58c07dd35
yngwie <yngwie_mariman@hotmail.com>
2021-02-25 12:08:38 +0100

    26.8

---
+++ src/main/java/be/vdab/bootsrap/controllers/IndexController.java
@@ -0,0 +1,20 @@
+package be.vdab.bootsrap.controllers;
+
+import be.vdab.bootsrap.domain.Persoon;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+class IndexController {
+    @GetMapping
+    public ModelAndView index() {
+        return new ModelAndView("index", "personen", new Persoon[]{
+                new Persoon(1, "Eddy", "Wally", "Ertvelde"),
+                new Persoon(2, "Johny", "Turbo", "Kortrijk"),
+                new Persoon(3, "Bobbejaan", "Schoepen", "Turnhout")
+        });
+    }
+}
